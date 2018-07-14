08aa06c1dd82cf3a3b61c65b83f644d36ec5fd38
James Kim <jhlink@users.noreply.github.com>
2018-07-14 06:14:04 -0400

    hackFeat: Implement VideoAudio fix for 360 video player

--- L7_SpatialAudio/Unity/SpatialAudioTest/Assets/Scripts/VideoControl.cs
+++ L7_SpatialAudio/Unity/SpatialAudioTest/Assets/Scripts/VideoControl.cs
@@ -6,42 +6,41 @@ public class VideoControl : MonoBehaviour {
 
     private UnityEngine.Video.VideoPlayer videoPlayer;
 
-
     [SerializeField]
     private AudioSource audioSource;
 
     void Start () {
         videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer> ();
+		audioSource = gameObject.AddComponent<AudioSource>();
 
 
         if (videoPlayer.clip != null)
         {
-            videoPlayer.EnableAudioTrack (0, true);
+			videoPlayer.audioOutputMode = UnityEngine.Video.VideoAudioOutputMode.AudioSource;
+			videoPlayer.EnableAudioTrack(0, true);
+//			audioSource.volume = 1.0f;
             videoPlayer.SetTargetAudioSource(0, audioSource);
+			videoPlayer.controlledAudioTrackCount = 1;
+			videoPlayer.Prepare ();
         }
     }
 
     //Check if input keys have been pressed and call methods accordingly.
     void Update(){
         //Play or pause the video.
-        if (Input.GetKeyDown ("space"))
-        {
-            if (videoPlayer.isPlaying)
-                videoPlayer.Pause ();
-            else
-                videoPlayer.Play();
-                audioSource.Play();
-        }
 
     }
 
 	public void toggleVideoState() {
-      if (videoPlayer.isPlaying) {
+	  if (videoPlayer.isPlaying) {
         videoPlayer.Pause ();
       } else {
         videoPlayer.Play();
-        audioSource.Play();
       }
+	}
+
+	public void restartVideoState() {
+		videoPlayer.Stop ();
 	}
 		
 }
